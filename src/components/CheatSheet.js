9e62e8e300f010f6e2c165e1a92ae3474e1e3a45
SharukhGV <sharukhvelupillai@pursuit.org>
2023-05-19 12:43:27 -0400

    table whitespace

--- src/components/CheatSheet.js
+++ src/components/CheatSheet.js
@@ -46,35 +46,35 @@ if (toggle) {
     </thead>
       <tbody>
 
- <tr><td>✨Acrab</td>  <td>Subgiant✨</td></tr>
-<tr><td>✨Al Satrb</td> <td>Subgiant✨</td></tr>
-<tr><td>✨Algorab</td>  <td>Subgiant✨</td></tr>
-<tr><td>✨Alnilam</td>  <td>Supergiant✨</td></tr>
-<tr><td>✨Alnitak</td>  <td>Supergiant✨</td></tr>
-<tr><td>✨Alshain</td>  <td>Subgiant✨</td></tr>
-<tr><td>✨Altair</td>  <td>Main Sequence✨</td></tr>
-<tr><td>✨Altais</td>  <td>Giant✨</td></tr>
-<tr><td>✨Antares</td>  <td>Supergiant✨</td></tr>
-<tr><td>✨Denebola</td>  <td>Main Sequence✨</td></tr>
-<tr><td>✨Dschubba</td> <td> Main Sequence✨</td></tr>
-<tr><td>✨Etamin</td> <td> Giant✨</td></tr>
-<tr><td>✨Gienah</td> <td> Giant✨</td></tr>
-<tr><td>✨Gorgonea Tertia</td>  <td>Bright Giant✨</td></tr>
-<tr><td>✨Kochab</td>  <td>Giant✨</td></tr>
-<tr><td>✨Kraz</td>  <td>Main Sequence✨</td></tr>
+ <tr><td>✨Acrab</td><td>Subgiant✨</td></tr>
+<tr><td>✨Al Satrb</td><td>Subgiant✨</td></tr>
+<tr><td>✨Algorab</td><td>Subgiant✨</td></tr>
+<tr><td>✨Alnilam</td><td>Supergiant✨</td></tr>
+<tr><td>✨Alnitak</td><td>Supergiant✨</td></tr>
+<tr><td>✨Alshain</td><td>Subgiant✨</td></tr>
+<tr><td>✨Altair</td><td>Main Sequence✨</td></tr>
+<tr><td>✨Altais</td><td>Giant✨</td></tr>
+<tr><td>✨Antares</td><td>Supergiant✨</td></tr>
+<tr><td>✨Denebola</td><td>Main Sequence✨</td></tr>
+<tr><td>✨Dschubba</td><td> Main Sequence✨</td></tr>
+<tr><td>✨Etamin</td><td> Giant✨</td></tr>
+<tr><td>✨Gienah</td><td> Giant✨</td></tr>
+<tr><td>✨Gorgonea Tertia</td><td>Bright Giant✨</td></tr>
+<tr><td>✨Kochab</td><td>Giant✨</td></tr>
+<tr><td>✨Kraz</td><td>Main Sequence✨</td></tr>
 <tr><td>✨Mintaka</td><td>Bright Giant✨</td></tr>
-<tr><td>✨Miram</td> <td>Supergiant✨</td></tr>
-<tr><td>✨Mirphak</td> <td> Subgiant✨</td></tr>
-<tr><td>✨Pherkad</td>  <td>Supergiant✨</td></tr>
-<tr><td>✨Polaris </td> <td>Supergiant✨</td></tr>
+<tr><td>✨Miram</td><td>Supergiant✨</td></tr>
+<tr><td>✨Mirphak</td><td> Subgiant✨</td></tr>
+<tr><td>✨Pherkad</td><td>Supergiant✨</td></tr>
+<tr><td>✨Polaris </td><td>Supergiant✨</td></tr>
 <tr><td>✨Rastaban </td><td> Bright Giant✨</td></tr>
-<tr><td>✨Regulus </td> <td>Main Sequence✨</td></tr>
-<tr><td>✨Rotanev</td>  <td>Giant✨</td></tr>
-<tr><td>✨Shetrak </td> <td>Giant✨</td></tr>
-<tr><td>✨Sualocin</td> <td> Giant✨</td></tr>
-<tr><td>✨Sulafat </td> <td>Giant✨</td></tr>
+<tr><td>✨Regulus </td><td>Main Sequence✨</td></tr>
+<tr><td>✨Rotanev</td><td>Giant✨</td></tr>
+<tr><td>✨Shetrak </td><td>Giant✨</td></tr>
+<tr><td>✨Sualocin</td><td> Giant✨</td></tr>
+<tr><td>✨Sulafat </td><td>Giant✨</td></tr>
 <tr><td>✨Tarazed </td><td> Bright Giant✨</td></tr>
-<tr><td>✨Vega </td> <td>Main Sequence✨</td></tr>
+<tr><td>✨Vega </td><td>Main Sequence✨</td></tr>
 <tr><td>✨Zosma </td><td> Main Sequence✨</td></tr>
 </tbody>
 
